5f4cbb6b9b35a4b4816a49af93294e2caa37bf81
Fred <95238688+Fredarik@users.noreply.github.com>
2026-03-27 17:02:00 +0100

    Обернення компонентів програми в ErrorBoundary для обробки помилок

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import logger from './utils/logger'
+import ErrorBoundary from './components/ErrorBoundary'
+import { setupGlobalErrorHandlers } from './utils/errorHandler'
+
+// Налаштування глобальних слухачів 
+setupGlobalErrorHandlers();
 
 const bootstrapLogger = logger('Main');
 bootstrapLogger.info('Ініціалізація та запуск React-додатка...');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
