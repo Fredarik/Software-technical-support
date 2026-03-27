9143396469f25d0422a9f95eedba6fd2ab962a64
Fred <95238688+Fredarik@users.noreply.github.com>
2026-03-27 16:48:56 +0100

    Додати ініціалізацію логера в main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logger from './utils/logger'
+
+const bootstrapLogger = logger('Main');
+bootstrapLogger.info('Ініціалізація та запуск React-додатка...');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
